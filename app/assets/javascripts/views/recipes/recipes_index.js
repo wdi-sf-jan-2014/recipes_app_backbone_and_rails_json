e5102fe7ef8456ec40b62c7ba9b41445130304d1
Alex Notov <anotov@gmail.com>
2014-03-07 11:39:06 -0800

    recipes form and submission handler stub

--- app/assets/javascripts/views/recipes/recipes_index.js
+++ app/assets/javascripts/views/recipes/recipes_index.js
@@ -2,6 +2,10 @@ RecipesWithBackboneJs.Views.RecipesIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['recipes/index'],
 
+  events: {
+    'submit #new_recipe': 'create_recipe'
+  },
+
   initialize: function() {
     this.collection.on('reset', this.render, this);
   },
@@ -9,6 +13,10 @@ RecipesWithBackboneJs.Views.RecipesIndex = Backbone.View.extend({
   render: function() {
     $(this.el).html(this.template({ recipes: this.collection.toJSON() }));
     return this;
+  },
+
+  create_recipe: function(event) {
+    event.preventDefault();
   }
 
 });
