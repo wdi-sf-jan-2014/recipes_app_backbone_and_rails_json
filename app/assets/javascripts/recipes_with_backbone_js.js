17a533c700f6fe7fb8a16a3b298aa73807e5214b
Alex Notov <anotov@gmail.com>
2014-03-07 10:19:57 -0800

    backbone-on-rails added

---
+++ app/assets/javascripts/recipes_with_backbone_js.js
@@ -0,0 +1,13 @@
+window.RecipesWithBackboneJs = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  RecipesWithBackboneJs.initialize();
+});
