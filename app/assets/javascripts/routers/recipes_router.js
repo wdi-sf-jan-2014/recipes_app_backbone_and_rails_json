46004cc01343a5409b18f32eabe1b31a897ec32f
Alex Notov <anotov@gmail.com>
2014-03-07 16:03:18 -0800

    Now showing a recipe

--- app/assets/javascripts/routers/recipes_router.js
+++ app/assets/javascripts/routers/recipes_router.js
@@ -1,6 +1,7 @@
 RecipesWithBackboneJs.Routers.Recipes = Backbone.Router.extend({
   routes: { 
-    'recipes': 'index'
+    'recipes': 'index',
+    'recipes/:id': 'show'
   },
 
   initialize: function() {
@@ -14,5 +15,19 @@ RecipesWithBackboneJs.Routers.Recipes = Backbone.Router.extend({
     );
 
     $('#container').html(view.render().el);
+  },
+
+  show: function(id) {
+    var recipe_id = parseInt(id, 10), recipe;
+    var self = this; 
+    this.collection.fetch({
+      success: function () {
+        recipe = self.collection.get(recipe_id);
+        var view = new RecipesWithBackboneJs.Views.Recipe(
+          { model: recipe }
+        );
+        $('#container').html(view.render().el);
+      }
+    });
   }
 });
